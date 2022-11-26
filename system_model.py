474188b47ee8f7328a77cff11fb75723d7501d8c
omagaDL <57490476+omagaDL@users.noreply.github.com>
2022-11-26 23:30:48 +0300

    Add files via upload

---
+++ system_model.py
@@ -0,0 +1,61 @@
+
+gravitational_constant = 6.67408E-11
+"""Гравитационная постоянная Ньютона G"""
+
+
+def calculate_force(body, space_objects):
+    """Вычисляет силу, действующую на тело.
+
+    Параметры:
+
+    **body** — тело, для которого нужно вычислить дейстующую силу.
+    **space_objects** — список объектов, которые воздействуют на тело.
+    """
+
+    body.Fx = body.Fy = 0
+    for obj in space_objects:
+        if body == obj:
+            continue  # тело не действует гравитационной силой на само себя!
+        r = ((body.x - obj.x)**2 + (body.y - obj.y)**2)**0.5
+        F = gravitational_constant * body.m * obj.m / r**2
+        body.Fx += F * (obj.x - body.x)/r
+        body.Fy += F * (obj.y - body.y)/r
+
+
+def move_space_object(body, dt, dist_x, dist_y, speeds, i):
+    """Перемещает тело в соответствии с действующей на него силой.
+
+    Параметры:
+
+    **body** — тело, которое нужно переместить.
+    """
+
+    ax = body.Fx / body.m
+    ay = body.Fy / body.m
+    body.Vx += ax * dt
+    body.x += body.Vx * dt
+    body.Vy += ay * dt
+    body.y += body.Vy * dt
+    dist_x[i].append(body.x)
+    dist_y[i].append(body.y)
+    speeds[i].append((body.Vy ** 2 + body.Vx ** 2) ** 0.5)
+
+
+def recalculate_space_objects_positions(space_objects, dt, dist_x, dist_y, speeds):
+    """Пересчитывает координаты объектов.
+
+    Параметры:
+
+    **space_objects** — список оьъектов, для которых нужно пересчитать координаты.
+    **dt** — шаг по времени
+    """
+
+    for body in space_objects:
+        calculate_force(body, space_objects)
+    for i in range(len(space_objects)):
+        body = space_objects[i]
+        move_space_object(body, dt, dist_x, dist_y, speeds, i=i)
+
+
+if __name__ == "__main__":
+    print("This module is not for direct call!")
